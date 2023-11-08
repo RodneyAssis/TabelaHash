dbb3a6db44e9e6c685d15959daea7c168d4a00df
Rodney Assis <rodney.assis@hotmail.com>
2023-11-08 16:19:55 -0300

    Add dados, tratamento e retorno

--- project/src/app.java
+++ project/src/app.java
@@ -1,11 +1,59 @@
 import person.Paciente;
 
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class app {
     public static void main(String[] args) {
-        var hr = new HashMap<Integer, Paciente>();
+        HashMap<Integer, Paciente> hr = new HashMap<>();
+        var input = new Scanner(System.in);
 
-        
+        int cont = 0;
+        // fazer com que esse while ele gere pacientes aleatorios use o gpt pra gerar
+        while (true) {
+            System.out.println("Para finalizar operação digite");
+            var opcao = input.next();
+            if (Objects.equals(opcao, "0")) {
+                break;
+            }
+
+            //Entreda dos dados
+            var idPaciente = cont;
+            System.out.println("Nome Paciente: ");
+            var nomePaciente = input.next();
+            System.out.println("Data de nascimento Paciente: ");
+            var dataNascPaciente = input.nextInt();
+            while (dataNascPaciente < 10000000 || dataNascPaciente > 99999999) {
+                System.out.println("Valor invalido. Por favor digite outra data valor:");
+                dataNascPaciente = input.nextInt();
+            }
+            System.out.println("Endereço do Paciente: ");
+            var enderecoPaciente = input.next();
+            System.out.println("telefone do Paciente: ");
+            var telefonePaciente = input.next();
+            System.out.println("Responsavel pelo Paciente: ");
+            var respPaciente = input.next();
+            System.out.println("Numero do responsavel pelo Paciente: ");
+            var numRespPaciente = input.next();
+            System.out.println("Possui alergia Paciente: ");
+            var isAlergicoPaciente = input.nextBoolean();
+
+            //instanciamento (alguns tratamentos são feitos na classe)
+            Paciente paciente = new Paciente(idPaciente, nomePaciente, dataNascPaciente,
+                    enderecoPaciente, telefonePaciente, respPaciente, numRespPaciente, isAlergicoPaciente);
+
+            hr.put(paciente.getId(), paciente);
+            cont++;
+        }
+
+        while (true) {
+            System.out.println("Digite o numero de consulta");
+            var opcao = input.nextInt();
+            System.out.println(hr.get(opcao));
+            if (opcao == -1) {
+                break;
+            }
+        }
     }
 }
--- project/src/person/Paciente.java
+++ project/src/person/Paciente.java
@@ -3,36 +3,68 @@ package person;
 import java.util.Objects;
 
 public class Paciente {
-    private int id;
-    private String name;
+    private final int id;
+    private final String name;
+    private final String dataNasc;
+    private final String endereco;
+    private final String numeroTel;
+    private final String responsavel;
+    private final String numResponsavel;
+    private final boolean isAlergico;
 
-    public Paciente(int id, String name) {
+    public Paciente(int id, String name, int dataNasc, String endereco,
+                    String numeroTel, String responsavel,
+                    String numResponsavel, boolean isAlergico) {
         this.id = id;
         this.name = name;
+        this.dataNasc = formatarData(String.valueOf(dataNasc));
+        this.endereco = endereco;
+        this.numeroTel = formatarNumeroTelefone(numeroTel);
+        this.responsavel = responsavel;
+        this.numResponsavel = formatarNumeroTelefone(numResponsavel);
+        this.isAlergico = isAlergico;
+    }
+
+    public String formatarData(String data) {
+        if (data.length() != 8) {
+            return "Data inválida";
+        }
+
+        String dia = data.substring(0, 2);
+        String mes = data.substring(2, 4);
+        String ano = data.substring(4);
+
+        return dia + "/" + mes + "/" + ano;
+    }
+
+    public String formatarNumeroTelefone(String numeroTel) {
+        String numeroLimpo = numeroTel.replaceAll("[^0-9]", "");
+
+        if (numeroLimpo.length() < 10) {
+            return "Número de telefone inválido";
+        }
+
+        String codigoArea = numeroLimpo.substring(0, 2);
+        String prefixo = numeroLimpo.substring(2, 7);
+        String sufixo = numeroLimpo.substring(7);
+
+        return String.format("(%s) %s-%s", codigoArea, prefixo, sufixo);
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String toString() {
-        return "Paciente{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return  "id: " + id + '\n' +
+                "name: " + name + '\n' +
+                "Nascimento: " + dataNasc + '\n' +
+                "Endereco: " + endereco + '\n' +
+                "Número de contato: " + numeroTel + '\n' +
+                "Responsavel: " + responsavel + '\n' +
+                "Número do responsavel: " + numResponsavel + '\n' +
+                "Possui alguma alergia? " + isAlergico;
     }
 
     @Override
@@ -40,11 +72,11 @@ public class Paciente {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Paciente paciente = (Paciente) o;
-        return id == paciente.id && Objects.equals(name, paciente.name);
+        return id == paciente.id && isAlergico == paciente.isAlergico && Objects.equals(name, paciente.name) && Objects.equals(dataNasc, paciente.dataNasc) && Objects.equals(endereco, paciente.endereco) && Objects.equals(numeroTel, paciente.numeroTel) && Objects.equals(responsavel, paciente.responsavel) && Objects.equals(numResponsavel, paciente.numResponsavel);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, dataNasc, endereco, numeroTel, responsavel, numResponsavel, isAlergico);
     }
 }
