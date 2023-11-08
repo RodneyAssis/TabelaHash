72b6e92dc3d323d8ead681c08447f2439d99b2c0
Rodney Assis <rodney.assis@hotmail.com>
2023-11-08 08:59:05 -0300

    Add class e main
    adição da classe paciente e o main para comunicação

---
+++ project/src/app.java
@@ -0,0 +1,11 @@
+import person.Paciente;
+
+import java.util.HashMap;
+
+public class app {
+    public static void main(String[] args) {
+        var hr = new HashMap<Integer, Paciente>();
+
+        
+    }
+}
---
+++ project/src/person/Paciente.java
@@ -0,0 +1,50 @@
+package person;
+
+import java.util.Objects;
+
+public class Paciente {
+    private int id;
+    private String name;
+
+    public Paciente(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Paciente paciente = (Paciente) o;
+        return id == paciente.id && Objects.equals(name, paciente.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
