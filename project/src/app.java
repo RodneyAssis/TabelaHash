8f0bd21f8f2845634277c75f15953ac35b48ceb4
Ricardo Perez <ricardoperezgalindo@gmail.com>
2023-11-10 15:39:34 -0300

    Comentei seu codigo e adicionei as tabelas hash em uma lista para consulta.

--- project/src/app.java
+++ project/src/app.java
@@ -1,59 +1,78 @@
 import person.Paciente;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class app {
     public static void main(String[] args) {
-        HashMap<Integer, Paciente> hr = new HashMap<>();
-        var input = new Scanner(System.in);
 
-        int cont = 0;
-        // fazer com que esse while ele gere pacientes aleatorios use o gpt pra gerar
-        while (true) {
-            System.out.println("Para finalizar operação digite");
-            var opcao = input.next();
-            if (Objects.equals(opcao, "0")) {
-                break;
-            }
+        Hospital hospital = new Hospital();
+        Paciente paciente1 = new Paciente(1,"Ricardo",10071993, "rua a","123456789","Dr.ruan","147258369",false);
+        Paciente paciente2 = new Paciente(2,"Rodney",12021998, "avpaulomaialopes","(79) 99860-2185","1","147258369",true);
+        Paciente paciente3 = new Paciente(3,"Gilberto",14061999, "rua b","(79) 99860-3040","2","147258369",true);
 
-            //Entreda dos dados
-            var idPaciente = cont;
-            System.out.println("Nome Paciente: ");
-            var nomePaciente = input.next();
-            System.out.println("Data de nascimento Paciente: ");
-            var dataNascPaciente = input.nextInt();
-            while (dataNascPaciente < 10000000 || dataNascPaciente > 99999999) {
-                System.out.println("Valor invalido. Por favor digite outra data valor:");
-                dataNascPaciente = input.nextInt();
-            }
-            System.out.println("Endereço do Paciente: ");
-            var enderecoPaciente = input.next();
-            System.out.println("telefone do Paciente: ");
-            var telefonePaciente = input.next();
-            System.out.println("Responsavel pelo Paciente: ");
-            var respPaciente = input.next();
-            System.out.println("Numero do responsavel pelo Paciente: ");
-            var numRespPaciente = input.next();
-            System.out.println("Possui alergia Paciente: ");
-            var isAlergicoPaciente = input.nextBoolean();
+        ArrayList<HashMap<String,Object>> pacientes = new ArrayList<>();
+        pacientes.add(hospital.cadastrarPaciente(paciente1));
+        pacientes.add(hospital.cadastrarPaciente(paciente2));
+        pacientes.add(hospital.cadastrarPaciente(paciente3));
 
-            //instanciamento (alguns tratamentos são feitos na classe)
-            Paciente paciente = new Paciente(idPaciente, nomePaciente, dataNascPaciente,
-                    enderecoPaciente, telefonePaciente, respPaciente, numRespPaciente, isAlergicoPaciente);
+        Random random = new Random();
+        int valor = random.nextInt(3);
 
-            hr.put(paciente.getId(), paciente);
-            cont++;
-        }
+        System.out.println(pacientes.get(valor));
 
-        while (true) {
-            System.out.println("Digite o numero de consulta");
-            var opcao = input.nextInt();
-            System.out.println(hr.get(opcao));
-            if (opcao == -1) {
-                break;
-            }
-        }
+
+
+
+
+        // HashMap<Integer, Paciente> hr = new HashMap<>();
+//        var input = new Scanner(System.in);
+
+//        int cont = 0;
+//        // fazer com que esse while ele gere pacientes aleatorios use o gpt pra gerar
+//        while (true) {
+//            System.out.println("Para finalizar operação digite");
+//            var opcao = input.next();
+//            if (Objects.equals(opcao, "0")) {
+//                break;
+//            }
+
+//            //Entreda dos dados
+//            var idPaciente = cont;
+//            System.out.println("Nome Paciente: ");
+//            var nomePaciente = input.next();
+//            System.out.println("Data de nascimento Paciente: ");
+//            var dataNascPaciente = input.nextInt();
+//            while (dataNascPaciente < 10000000 || dataNascPaciente > 99999999) {
+//                System.out.println("Valor invalido. Por favor digite outra data valor:");
+//                dataNascPaciente = input.nextInt();
+//            }
+//            System.out.println("Endereço do Paciente: ");
+//            var enderecoPaciente = input.next();
+//            System.out.println("telefone do Paciente: ");
+//            var telefonePaciente = input.next();
+//            System.out.println("Responsavel pelo Paciente: ");
+//            var respPaciente = input.next();
+//            System.out.println("Numero do responsavel pelo Paciente: ");
+//            var numRespPaciente = input.next();
+//            System.out.println("Possui alergia Paciente: ");
+//            var isAlergicoPaciente = input.nextBoolean();
+//
+//            //instanciamento (alguns tratamentos são feitos na classe)
+//            Paciente paciente = new Paciente(idPaciente, nomePaciente, dataNascPaciente,
+//                    enderecoPaciente, telefonePaciente, respPaciente, numRespPaciente, isAlergicoPaciente);
+
+//            hr.put(paciente.getId(), paciente);
+//            cont++;
+
+//        while (true) {
+//            System.out.println("Digite o numero de consulta");
+//            System.out.println(hr.get(opcao));
+//            if (opcao == -1) {
+//                break;
+//            }
+//        }
     }
-}
+
+
+  }
+
